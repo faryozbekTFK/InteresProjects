07079ddb7d944fffe6ec249000fc77395ff1fa40
faryozbekTFK <t.faryozbek1311@gmail.com>
2021-07-26 13:20:22 +0500

    Hidden Search Widget

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
-const CONTENER = document.querySelector(".searchCont");
+const SEARCH = document.querySelector(".search");
 const INPUT = document.getElementById("input");
-const BUTTON = document.getElementById("btn");
-const CLEAR = document.getElementById("clear");
-let clk = 0;
-BUTTON.addEventListener("click", () => {
+const BTN = document.getElementById("btn");
+
+BTN.addEventListener("click", () => {
   if (INPUT.value) {
     console.log(INPUT.value);
-  } else if (!clk) {
-    INPUT.classList.remove("search2");
-    INPUT.classList.add("search");
-    BUTTON.classList.add("btnAct");
-    clk = 1;
   } else {
-    INPUT.classList.remove("search");
-    INPUT.classList.add("search2");
-    BUTTON.classList.remove("btnAct");
-    clk = 0;
+    SEARCH.classList.toggle("active");
   }
-});
-INPUT.addEventListener("keyup", () => {
-  if (INPUT.value) CLEAR.classList.add("clearActive");
-  else CLEAR.classList.remove("clearActive");
-});
-CLEAR.addEventListener("click", () => {
-  INPUT.value = "";
-  CLEAR.classList.remove("clearActive");
 });
