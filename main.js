ac17c2a7a6fba0ea7c320ba8168d8d3573be4d6a
faryozbekTFK <t.faryozbek1311@gmail.com>
2021-08-05 15:54:45 +0500

    Kvadrat tenglama

---
+++ main.js
@@ -0,0 +1,33 @@
+const A = document.getElementById("a");
+const B = document.getElementById("b");
+const C = document.getElementById("c");
+const RESULT = document.getElementById("result");
+const BTN = document.getElementById("btn");
+
+function KvTenglama() {
+  let D = 0,
+    x1 = 0,
+    x2 = 0,
+    x = 0;
+  Aval = parseInt(A.value);
+  Bval = parseInt(B.value);
+  Cval = parseInt(C.value);
+  D = Math.pow(Bval, 2) - 4 * Aval * Cval;
+  if (D > 0) {
+    x1 = (-Bval - Math.sqrt(D)) / (2 * Aval);
+    x2 = (-Bval + Math.sqrt(D)) / (2 * Aval);
+    RESULT.innerText = `D = ${D}
+                        x1 = ${x1}
+                        x2 = ${x2}`;
+  } else if (D == 0) {
+    x = -Bval / (2 * Aval);
+    RESULT.innerText = `D = ${D}
+                            x = ${x}`;
+  } else
+    RESULT.innerText = `D = ${D}
+             Tenglama ildizga ega emas.`;
+}
+
+BTN.onclick = function () {
+  KvTenglama();
+};
